83a6b9213c4b3e220c373765fca5e400d0b9fe86
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-12 14:37:21 +0200

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+
+const galleryBase = document.querySelector('.gallery');
+
+
+for (const item of galleryItems) {
+    const galEl = 
+    `<li>
+    <a class="gallery__item" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`;
+ galleryBase.insertAdjacentHTML('beforeend', galEl)
+}
+const lightBox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData:"alt",
+    showCounter: true,
+    scrollbarWidth: 20
+})
+
