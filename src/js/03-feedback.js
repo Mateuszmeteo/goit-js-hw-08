d30411406f821d81ac7c0dd1e094efdc5958e99e
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-13 20:29:49 +0200

    problems2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,15 +5,23 @@ const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-updateOutput();
-form.addEventListener('submit', saveMessage);
 
-function saveMessage(e) {
-    e.preventDefault();
-    localStorage.setItem(LOCALSTORAGE_KEY, form.element.message.value);
-    updateOutput();
-    form.reset();
-}
-function updateOutput() {
-    output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-  }
+
+
+
+
+
+
+
+// updateOutput();
+// form.addEventListener('submit', saveMessage);
+
+// function saveMessage(e) {
+//     e.preventDefault();
+//     localStorage.setItem(LOCALSTORAGE_KEY, form.element.message.value);
+//     updateOutput();
+//     form.reset();
+// }
+// function updateOutput() {
+//     output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
+//   }
