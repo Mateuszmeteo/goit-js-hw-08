1025675cac9584ae84c5a12009e5bf7353ef4169
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-14 12:48:21 +0200

    problems4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,16 +3,28 @@ import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form');
+const formEl = document.querySelector('.feedback-form');
 const emailEl = document.querySelector('input[name = "email"]');
 const messageEl = document.querySelector('textarea[name = "message"]');
 
 
 
+const feedback = {
+  email: emailEl.value,
+  message: messageEl.value
+}
 
+function addLocalStorage () {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback))
 
+}
 
+const throttledAddToLocalStorage = throttle(addToLocalStorage, 500)
 
+window.addEventListener('submit', (e)) = {
+    
+
+}
 
 
 // updateOutput();
