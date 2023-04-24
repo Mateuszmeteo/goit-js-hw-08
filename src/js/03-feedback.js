5cd034fdf4b85c7eaab3693e870b144a88e27627
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-04-24 14:57:34 +0200

    last

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,25 +9,80 @@ const messageEl = document.querySelector('textarea[name = "message"]');
 
 // const submitEl = document.querySelector('button[type = "submit"]')
 
-
+///zapis danyc do form///
 
 const addLocalStorage = () => {
   const feedback = {
   email: emailEl.value,
   message: messageEl.value
+};
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback))
 }
-}
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback))
-  // console.log(feedback)
-
-
-  const throttledAddLocalStorage = throttle (e => {
-    emailEl = input.value;
-    messageEl = input.value;
-    localStorage.setItem(JSON.stringify({emailEl, messageEl}))
-  },500)
   
-  form.addEventListener('input', throttledAddLocalStorage)
+
+///// fynkcja wczytujaca dane do localstorage i wypełnia pola form
+
+const loadLocalStorage = () => {
+  const feedback =
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+  emailEl.value = feedback.email || '';
+  messageEl.value = feedback.message || '';
+};
+
+
+/////// funkcja czyszczaca localstorage i pola form po wysłaniu form
+
+
+const clearLocalStorage = () => {
+  const feedback =
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  emailEl.value = '';
+  messageEl.value = '';
+  console.log(feedback);
+};
+
+
+//////// sledzenie zdarzenia na input i zapis stanu do localstorage
+
+emailEl.addEventListener('input', throttle(addLocalStorage, 500));
+messageEl.addEventListener('input', throttle(addLocalStorage, 500));
+
+////////// wczytywanie danych z localstorage po przeladowaniu strony
+
+document.addEventListener('DOMContentLoaded', loadLocalStorage);
+
+
+////// czyszczenie danych po wysłaniu form 
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  clearLocalStorage();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+
+  // const throttledAddLocalStorage = throttle (e => {
+  //   emailEl = input.value;
+  //   messageEl = input.value;
+  //   localStorage.setItem(JSON.stringify({emailEl, messageEl}))
+  // },500)
+  
+  // form.addEventListener('input', throttledAddLocalStorage)
   
   
   
